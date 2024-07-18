a07d1b2d25f74fcacfaa966a50516da742ca6c14
TELAPROLU LAKSHMI SRINIVAS <109592965+srinivastls@users.noreply.github.com>
2024-07-18 15:47:39 +0530

    add3

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tabula
 import os
+from jnius import autoclass
 
-# Set up JAVA_HOME environment variable
-os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
-os.environ["PATH"] = os.environ["PATH"] + ":" + os.path.join(os.environ["JAVA_HOME"], "bin")
+# Set up pyjnius to use the JVM bundled with it
+JavaSystem = autoclass('java.lang.System')
+JavaSystem.setProperty('java.home', '/usr/lib/jvm/java-8-openjdk-amd64')
 
 def extract_data(uploaded_file):
     tables = tabula.read_pdf(uploaded_file, pages="all", multiple_tables=True)
